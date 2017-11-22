9bd227a8af4497538ddfa093e5f3ddb870a461b8
Omair <omairraza9@gmail.com>
2017-11-21 20:09:30 -0500

    DRY kitten code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,6 +15,14 @@ class PagesController < ApplicationController
   end
 
   def kitten
+    set_kitten_url
+  end
+
+  def kittens
+    set_kitten_url
+  end
+
+  def set_kitten_url
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
